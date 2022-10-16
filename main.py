31ec32ade94fa3bbecc77e58b92d176e33ac5a9d
malco <rkkrowicki@gmail.com>
2022-10-16 17:20:17 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
+
+
+def goals(laLiga, copaDelRey, championsLeague):
+    total_goals = laLiga + copaDelRey + championsLeague
+    return total_goals
+
+
+def find_average(numbers):
+    length = len(numbers)
+    if length == 0:
+        return 0
+    return sum(numbers)/length
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
